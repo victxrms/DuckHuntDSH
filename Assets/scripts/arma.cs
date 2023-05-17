16000992cdc2db68027f866ef2ab893c8a3233a2
elcarlillo <carlos.vidalro@alum.uca.es>
2023-05-17 18:36:01 +0200

    un monton de cosas sin terminar

---
+++ Assets/scripts/arma.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class arma : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    public Image balaC;
+    public Image NobalaC;
+    public Image balaT;
+    public Image NobalaT;
+
+
+    public Image balaCargador1;
+    public Image balaCargador2;
+    public Image balaCargador3;
+
+    public List<Image> balasCargador; // Lista para las im�genes de las balas del cargador
+    public int indiceBalaCargadorActual = 0; // �ndice de la bala actual en el cargador
+
+    
+
+    public List<Image> balasTotales;
+    public int indiceBalaTotalActual = 0;
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+
+            balaCargador1.sprite = NobalaC;
+
+            if (indiceBalaActual < balasTotales.Count)
+            {
+                balasTotales[indiceBalaActual].sprite = NobalaT;
+                indiceBalaActual++;
+            }
+
+            tiempoDesdeDisparo = 0f;
+        }
+    }
+}
---
+++ Assets/scripts/bala.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bala : MonoBehaviour
+{
+    public float life = 3;
+
+    void Awake()
+    {
+        // Apuntar la bala hacia adelante
+        transform.rotation = Quaternion.LookRotation(transform.forward);
+        Destroy(gameObject, life);
+    }
+
+    void OnTriggerEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    
+        if(collision.CompareTag("Pato"))
+        {
+            collision.SendMessage("Muerto");
+        }
+
+        if(collision.CompareTag("Puntos_extra"))
+        {
+            collision.SendMessage("Puntos_extra");
+        }
+
+        if(collision.CompareTag("Patos_lentos"))
+        {
+            collision.SendMessage("Patos_lentos");
+        }
+    }
+} 
